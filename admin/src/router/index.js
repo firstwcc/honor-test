3f65d188af53ce842b011402b2fd2d62eaa92b35
foreverwcc <1292855448@qq.com>
2020-09-16 21:57:42 +0800

    admin使用路由懒加载

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -4,37 +4,37 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
   const routes = [
-  {path:'/login',name:'login',component:import('../views/Login/Login.vue'),meta:{isPublic:true}},
+  {path:'/login',name:'login',component:() => import('../views/Login/Login.vue'),meta:{isPublic:true}},
   {
     path: '/',
     name: 'Main',
     component: import('../views/Main.vue'),
     children:[
-      {path:"/categories/create",component:import('../views/Category/CategoryEdit.vue')},
+      {path:"/categories/create",component:() => import('../views/Category/CategoryEdit.vue')},
       //点击编辑跳转到CategoryEdit页面，即使用相同的Category页面组件，
       //props表示"/categories/edit/:id"里的参数都注入到CategoryEdit页面里
-      {path:"/categories/edit/:id",component:import('../views/Category/CategoryEdit.vue'),props:true},
-      {path:'/categories/list',component:import('../views/Category/CategoryList.vue')},
+      {path:"/categories/edit/:id",component:() => import('../views/Category/CategoryEdit.vue'),props:true},
+      {path:'/categories/list',component:() => import('../views/Category/CategoryList.vue')},
       
-      {path:'/items/create',component:import('../views/Item/ItemEdit.vue')},
-      {path:'/items/edit/:id',component:import('../views/Item/ItemEdit.vue'),props:true},
-      {path:'/items/list',component:import('../views/Item/ItemList.vue')},
+      {path:'/items/create',component:() => import('../views/Item/ItemEdit.vue')},
+      {path:'/items/edit/:id',component:() => import('../views/Item/ItemEdit.vue'),props:true},
+      {path:'/items/list',component:() => import('../views/Item/ItemList.vue')},
 
-      {path:'/heroes/create',component:import('../views/Hero/HeroEdit.vue')},
-      {path:'/heroes/edit/:id',component:import('../views/Hero/HeroEdit.vue'),props:true},
-      {path:'/heroes/list',component:import('../views/Hero/HeroList.vue')},
+      {path:'/heroes/create',component:() => import('../views/Hero/HeroEdit.vue')},
+      {path:'/heroes/edit/:id',component:() => import('../views/Hero/HeroEdit.vue'),props:true},
+      {path:'/heroes/list',component:() => import('../views/Hero/HeroList.vue')},
 
-      {path:'/articles/create',component:import('../views/Article/ArticleEdit.vue')},
-      {path:'/articles/edit/:id',component:import('../views/Article/ArticleEdit.vue'),props:true},
-      {path:'/articles/list',component:import('../views/Article/ArticleList.vue')},
+      {path:'/articles/create',component:() => import('../views/Article/ArticleEdit.vue')},
+      {path:'/articles/edit/:id',component:() => import('../views/Article/ArticleEdit.vue'),props:true},
+      {path:'/articles/list',component:() => import('../views/Article/ArticleList.vue')},
 
-      {path:'/ads/create',component:import('../views/Ad/AdEdit.vue')},
-      {path:'/ads/edit/:id',component:import('../views/Ad/AdEdit.vue'),props:true},
-      {path:'/ads/list',component:import('../views/Ad/AdList.vue')},
+      {path:'/ads/create',component:() => import('../views/Ad/AdEdit.vue')},
+      {path:'/ads/edit/:id',component:() => import('../views/Ad/AdEdit.vue'),props:true},
+      {path:'/ads/list',component:() => import('../views/Ad/AdList.vue')},
 
-      {path:'/admin_users/create',component:import('../views/AdminUser/AdminUserEdit.vue')},
-      {path:'/admin_users/edit/:id',component:import('../views/AdminUser/AdminUserEdit.vue'),props:true},
-      {path:'/admin_users/list',component:import('../views/AdminUser/AdminUserList.vue')},
+      {path:'/admin_users/create',component:() => import('../views/AdminUser/AdminUserEdit.vue')},
+      {path:'/admin_users/edit/:id',component:() => import('../views/AdminUser/AdminUserEdit.vue'),props:true},
+      {path:'/admin_users/list',component:() => import('../views/AdminUser/AdminUserList.vue')},
   ]
   }
 ]
